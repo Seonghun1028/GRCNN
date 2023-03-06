d252e87d8891b14b61aa16a2a43b15e144822b65
cktjdgns98 <cktjdgns98@hanyang.ac.kr>
2023-03-06 16:41:10 +0900

    230306

--- devel/local_setup.zsh
+++
@@ -1,1 +0,0 @@
-/home/csh/csh_ws/src/grcnn/devel/.private/catkin_tools_prebuild/local_setup.zsh
--- devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/csh/csh_ws/src/grcnn/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
